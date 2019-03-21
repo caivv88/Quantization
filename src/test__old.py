0f8464487cca45471cf4f6efd2ba5621981e5249
caivv88 <893032721@qq.com>
2019-03-22 00:44:04 +0800

    测试脚本

---
+++ src/test__old.py
@@ -0,0 +1,9 @@
+import tushare as ts
+
+data = ts.get_hist_data('300274')
+
+xinshougu = ts.xsg_data()
+
+yugao = ts.forecast_data(2019, 1)
+
+aa = ts.get_profit_data(2018, 4)
---
+++ src/test_new.py
@@ -0,0 +1,7 @@
+import tushare as ts
+
+ts.set_token('1516cda7ec78094604e39ddb55cd2ff7304ff251df9e5c5c863ecc27')  # set token
+pro = ts.pro_api()
+
+# 日线行情
+df = pro.daily(ts_code='000001.SZ', start_date='20160701', end_date='20190322')
