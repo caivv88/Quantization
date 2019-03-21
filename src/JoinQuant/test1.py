12f6a865be2e5af8aade455bf927ffedcf59504a
caivv88 <893032721@qq.com>
2019-03-22 01:15:41 +0800

    聚宽测试脚本

---
+++ src/JoinQuant/test1.py
@@ -0,0 +1,12 @@
+"""说明书
+https://www.joinquant.com/help/api/help?name=JQData"""
+from jqdatasdk import *
+
+auth('13530976495', 'hao122')  # 认证
+
+q = query(finance.STK_FIN_FORCAST).filter(finance.STK_FIN_FORCAST.code == '600519.XSHG',
+                                          finance.STK_FIN_FORCAST.pub_date >= '2018-01-01').limit(10)
+df = finance.run_query(q)
+
+"""动态情景多因子Alpha模型
+https://www.joinquant.com/view/community/detail/13717"""
