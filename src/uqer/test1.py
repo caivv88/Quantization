d72e938319e0f2adfabc15a2bd145b8b82243232
caivv88 <893032721@qq.com>
2019-03-26 01:14:45 +0800

    案例

---
+++ src/uqer/test1.py
@@ -0,0 +1,12 @@
+'''优矿'''
+'''https://uqer.io/v3/community/share/5b42d361f94728012fe93b95'''
+
+'''https://uqer.io/v3/community/share/5a702eb118408e010b3c1caf
+集成学习模型在选股方面的应用'''
+
+'''https://uqer.io/v3/community/share/5784b3d1228e5b8a09932d9e
+Fama-French 三因子模型在A股市场的实证研究'''
+
+
+'''https://uqer.io/v3/community/share/5b1a1260d52680015f105c11
+量化基础必修第六课：多因子策略从入门到实践'''
