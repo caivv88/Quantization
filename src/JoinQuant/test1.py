e69aef60fce3b207ad157f524d85d218f033ea0f
caivv88 <893032721@qq.com>
2019-04-01 23:33:48 +0800

    update 0401

--- src/JoinQuant/test1.py
+++ src/JoinQuant/test1.py
@@ -1,6 +1,7 @@
 """说明书
 https://www.joinquant.com/help/api/help?name=JQData"""
 from jqdatasdk import *
+from sqlalchemy import *
 
 auth('13530976495', 'hao122')  # 认证
 
@@ -10,3 +11,12 @@ df = finance.run_query(q)
 
 """动态情景多因子Alpha模型
 https://www.joinquant.com/view/community/detail/13717"""
+
+q = query(
+    valuation
+).filter(
+    valuation.code == '000001.XSHE'
+)
+df = get_fundamentals(q, '2018-10-15')
+# 打印出总市值
+print(df['market_cap'][0])
--- src/tushare/test_new.py
+++ src/tushare/test_new.py
@@ -5,3 +5,7 @@ pro = ts.pro_api()
 
 # 日线行情
 df = pro.daily(ts_code='000001.SZ', start_date='20160701', end_date='20190322')
+
+df1 = ts.get_operation_data(2018, 4)
+
+df2 = ts.get_today_all()
